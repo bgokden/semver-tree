27d03efef30d8e1fe07ada07da150598d6048d4e
berkgokden <berkgokden@gmail.com>
2020-01-26 13:47:39 +0100

    Added more tests

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.12
 require (
 	github.com/blang/semver v3.5.1+incompatible
 	github.com/google/btree v1.0.0
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/tools v0.0.0-20190911230505-6bfd74cf029c
 )
--- semvertree_test.go
+++ semvertree_test.go
@@ -7,13 +7,58 @@ import (
 	semvertree "github.com/bgokden/semver-tree"
 )
 
-func SemverTreeBasicTest(t *testing.T) {
+func TesSemverTreeBasic(t *testing.T) {
 	st := semvertree.New()
 	st.AddVersion("v1.0.1", map[string]string{"rule": "test 3"})
 	st.AddVersion("v2.0.0", map[string]string{"rule": "test 2"})
 	st.AddVersion("v1.0.0", map[string]string{"rule": "test 1"})
 	st.AddVersion("v2.0.1", map[string]string{"rule": "test 2"})
 	st.AddVersion("v2.0.3", map[string]string{"rule": "test 2"})
+	st.Rebuild()
+	fmt.Printf("----\n")
+	st.Walk(func(prev, value *semvertree.Value, degreeOfDifference int) {
+		if prev == nil {
+			fmt.Printf("root: %v\n", value.Version)
+			return
+		}
+		fmt.Printf("prev: %v next: %v degreeOfDifference: %v data: %v\n", prev.Version, value.Version, degreeOfDifference, value.Data)
+	})
+	fmt.Printf("----\n")
+}
+
+func TestSemverTreeBasic2(t *testing.T) {
+	st := semvertree.New()
+	st.AddVersion("v1.0.1", map[string]string{"rule": "test 3"})
+	st.AddVersion("v2.0.0", map[string]string{"rule": "test 2"})
+	st.AddVersion("v1.0.0", map[string]string{"rule": "test 1"})
+	st.AddVersion("v2.0.1", map[string]string{"rule": "test 2"})
+	st.AddVersion("v2.0.3", map[string]string{"rule": "test 2"})
+	st.AddVersion("v1.0.3", map[string]string{"rule": "test 4"})
+	st.AddVersion("v1.1.0", map[string]string{"rule": "test 4"})
+	st.AddVersion("v1.1.2", map[string]string{"rule": "test 5"})
+	st.Rebuild()
+	fmt.Printf("----\n")
+	st.Walk(func(prev, value *semvertree.Value, degreeOfDifference int) {
+		if prev == nil {
+			fmt.Printf("root: %v\n", value.Version)
+			return
+		}
+		fmt.Printf("prev: %v next: %v degreeOfDifference: %v data: %v\n", prev.Version, value.Version, degreeOfDifference, value.Data)
+	})
+	fmt.Printf("----\n")
+}
+
+func TestSemverTreeBasic3(t *testing.T) {
+	st := semvertree.New()
+	st.AddVersion("v1.0.1", map[string]string{"rule": "test 3"})
+	st.AddVersion("v2.0.0", map[string]string{"rule": "test 2"})
+	st.AddVersion("v1.0.0", map[string]string{"rule": "test 1"})
+	st.AddVersion("v2.0.1", map[string]string{"rule": "test 2"})
+	st.AddVersion("v2.0.3", map[string]string{"rule": "test 2"})
+	st.AddVersion("v1.0.3", map[string]string{"rule": "test 4"})
+	st.AddVersion("v1.1.0", map[string]string{"rule": "test 4"})
+	st.AddVersion("v1.1.2", map[string]string{"rule": "test 5"})
+	st.AddVersion("v3.0.1", map[string]string{"rule": "test 5"})
 	st.Rebuild()
 	fmt.Printf("----\n")
 	st.Walk(func(prev, value *semvertree.Value, degreeOfDifference int) {
